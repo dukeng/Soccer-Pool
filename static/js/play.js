3fb323951873ee1af4df41e349de01f53e50b29a
Duke Nguyen <nguyentranduc1@gmail.com>
2016-06-15 22:45:03 -0700

    Initial use with physics in Phaser

--- static/js/play.js
+++ static/js/play.js
@@ -5,9 +5,10 @@ var Play = function(game){
     //shortcuts for the constant 
     widthRatio = game.global.ratio;
     heightRatio = game.global.ratio;
-    scaleX = widthRatio * 0.1;
-    scaleY = heightRatio * 0.1;
+    scaleX = widthRatio * 0.07;
+    scaleY = heightRatio * 0.07;
     objectRatio = game.global.objectRatio;
+    var cursors;
 }
 
 
@@ -16,6 +17,9 @@ Play.prototype = {
 	create: function () {
 		var field = this.game.add.sprite(0, 0, 'field');
         var fieldRatio = field.width / field.height;
+        field.x = this.game.world.width / 2;
+        field.y =  this.game.world.height * (720/920)/2;
+        field.anchor.setTo(0.5,0.5);
         if (fieldRatio < widthRatio ) {
             field.height = this.game.world.height * (720/920);
             field.width = field.height * widthRatio;
@@ -23,12 +27,7 @@ Play.prototype = {
             field.width = this.game.world.width;
             field.height = field.width * 1/ widthRatio;
         }
-        field.x = this.game.world.width / 2;
-        field.y =  this.game.world.height * (720/920)/2;
-		// field.width = this.game.world.width;
-		// field.height = this.game.world.height * 1 / heightRatio;
-        field.anchor.setTo(0.5,0.5);
-        console.log("width is" + this.game.world.width);
+        
         players = this.game.add.group(); // this initialize a players group with missing params
         for (var i = 0; i < 11; i++) {
         	player = players.create(objectRatio * 30 * i, objectRatio * 30 * i, 'player1');
@@ -40,12 +39,39 @@ Play.prototype = {
         	player.scale.setTo(scaleX,scaleY);
             player.anchor.setTo(0.5, 0.5);
         };
+
+        // game.renderer.clearBeforeRender = false;
+        // game.renderer.roundPixels = true;
+
+        this.game.physics.startSystem(Phaser.Physics.ARCADE);
+
         ball = this.game.world.create(objectRatio * 640, objectRatio * 360, 'ball');
         ball.scale.setTo(scaleX,scaleY);
         ball.anchor.setTo(0.5, 0.5);
+        //enable physics mode on ball
+        this.game.physics.enable(ball, Phaser.Physics.ARCADE);
+        ball.body.drag.set(200);
+        ball.body.maxVelocity.set(400);
+        cursors = this.game.input.keyboard.createCursorKeys();
+        //this makes the game excluded from input of the browser
+        this.game.input.keyboard.addKeyCapture([ Phaser.Keyboard.SPACEBAR ]);
     },
 
     update: function () {
-
+        if (cursors.up.isDown){
+            this.game.physics.arcade.accelerationFromRotation(ball.rotation, 200, ball.body.acceleration);
+        }
+        else{
+            ball.body.acceleration.set(0);
+        }
+        if (cursors.left.isDown){
+            ball.body.angularVelocity = -300;
+        }
+        else if (cursors.right.isDown){
+            ball.body.angularVelocity = 300;
+        }
+        else{
+            ball.body.angularVelocity = 0;
+        }
     }
 }
